2f3a0ddf8dd4594b3ce3c076cc2850b25054f0c2
Lindell Carter <lindellcarternyc@gmail.com>
2021-02-21 18:06:36 -0500

    refactor and DRY components

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import MovieCard from './MovieCard';
 
 export default function MovieList(props) {
   const history = useHistory()
@@ -9,24 +10,8 @@ export default function MovieList(props) {
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} onClick={selectMovie}/>
+        <MovieCard key={movie.id} movie={movie} onClick={selectMovie}/>
       ))}
-    </div>
-  );
-}
-
-function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
-
-  return (
-    <div className="movie-card" onClick={() => props.onClick(props.movie.id)}>
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import './index.css';
 import App from './App';
 
 // You'll need to wrap <App /> for routing to work
 const wrappedApp = (
-    <BrowserRouter>
+    <Router>
         <App />
-    </BrowserRouter>
+    </Router>
 )
 ReactDOM.render(wrappedApp, document.getElementById('root'));
